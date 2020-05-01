e7f971b0d16c770942fa800da0570adc78713e9c
Oleg Smirnov <osmirnov@gmail.com>
2020-05-02 00:29:31 +0200

    fixing invocation of singularity mode, mount ~/.radiopadre

--- radiopadre_client/backends/singularity.py
+++ radiopadre_client/backends/singularity.py
@@ -103,6 +103,7 @@ def start_session(container_name, selected_ports, userside_ports, notebook_path,
     homedir = os.path.expanduser("~")
     docker_opts += [
         "-B", "{}:{}{}".format(ABSROOTDIR, ABSROOTDIR, ""), # ":ro" if orig_rootdir else ""),
+        "-B", "{}/.radiopadre:{}/.radiopadre".format(homedir, homedir),
         # hides /home/user/.local, which if exposed, can confuse jupyter and ipython
         "-B", "{}:{}/.local".format(docker_local, homedir),
         # mount session info directory (needed to serve e.g. js9prefs.js)
