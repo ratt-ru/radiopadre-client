0ed32a4f3a94eabbd62966be7267bab7cca9d4a2
Oleg Smirnov <osmirnov@gmail.com>
2023-11-03 00:34:11 +0200

    changed way of specifying hostPath

--- radiopadre_client/default_config.py
+++ radiopadre_client/default_config.py
@@ -105,13 +105,13 @@ DefaultConfig = OrderedDict(
     K8S_NAMESPACE = "",
     K8S_UID = -1,
     K8S_GID = -1,
-    K8S_HOSTPATH = False,
+    K8S_LOCAL_DISK = False,
     K8S_AUTO_CLEANUP = True,
     K8S_HOME_DIR = "",
     K8S_RADIOPADRE_DIR = "",
     K8S_NODE_SELECTOR = "",
-    K8S_CPU_REQUEST = "",
-    K8S_RAM_REQUEST = "",
+    K8S_CPU_REQUEST = "2",
+    K8S_RAM_REQUEST = "8Gi",
 
     # All of the options above can be persisted in the config file via --save-config-host or --save-config-session.
     # The options below are "one-shot" and non-persisting, they are not saved to the config. This is indicated by a
